6de6c8545b00e121b7085e9ee7e2d3684f413d8a
Cody <codybarker.or@gmail.com>
2023-05-02 14:42:02 -0700

    mock table

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="Crop Tracker"></header>
+      <header>Crop Tracker</header>
+      <h3>Farm Name and Location</h3>
       <table>
         <tr>
-          <td>Farm Name</td>
-          <td>Farm Location</td>
+          <th>Bed id</th>
+          <th>In Use</th>
+          <th>Crop</th>
+          <th>DTM</th>
+          <th>Planting Date</th>
+          <th>Harvest Date</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Yes</td>
+          <td>Radishes</td>
+          <td>29</td>
+          <td>04-01-23</td>
+          <td>4-30-23</td>
         </tr>
       </table>
     </div>
