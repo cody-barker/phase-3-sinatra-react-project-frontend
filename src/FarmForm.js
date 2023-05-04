d24c49c5c25a403a0ad1498db45e74bcf4c30471
Cody <codybarker.or@gmail.com>
2023-05-04 10:02:32 -0700

    creating form with handlers and state

--- src/FarmForm.js
+++ src/FarmForm.js
@@ -6,12 +6,25 @@ function FarmForm() {
     const [city, setCity] = useState("")
     const [state, setState] = useState("")
 
+    function onNameChange(e) {
+        setName(e.target.value)
+    }
+    
+    function onCityChange(e) {
+        setCity(e.target.value)
+    }
+
+    function onStateChange(e) {
+        setState(e.target.value)
+    }
+
 
     return (
         <form autoComplete="off">
-            <input type="text" placeholder="Farm Name"></input>
-            <input type="text" placeholder="City"></input>
-            <input type="text" placeholder="State"></input>
+            <input onChange={onNameChange} value={name} type="text" placeholder="Farm Name"></input>
+            <input onChange={onCityChange} value={city} type="text" placeholder="City"></input>
+            <input onChange={onStateChange} value={state} type="text" placeholder="State"></input>
+            <button type="submit">Submit Changes</button>
       </form>
     )
 }
